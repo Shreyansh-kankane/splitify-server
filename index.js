525b97ead18a53e9c8e91c0a7ad24da4fbfeebca
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2024-05-25 00:05:52 +0530

    authentication

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send("Splitify server up");
+});
+
 app.use('/user',authRoutes);
 
 const PORT = process.env.PORT || 5000;
@@ -19,12 +23,9 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
 }).then(() => {
     console.log("Connected to database");
-
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
-
-
 }).catch((err) => {
     console.log("Error connecting to database", err);
 });
